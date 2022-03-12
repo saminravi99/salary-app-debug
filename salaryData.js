425c892e77d4045337b8fb191a256b12a8632316
Samin Israr Ravi <saminisrar1@gmail.com>
2022-03-12 20:36:58 +0600

    first commit

---
+++ salaryData.js
@@ -0,0 +1,64 @@
+let salary_data = {
+  0: {
+    hobby: "talking javascript",
+    name: "Addy Osmani",
+    salary: "76340.0",
+  },
+  1: {
+    name: "Paul Irish",
+    salary: "94320.0",
+    status: "married",
+  },
+  2: {
+    name: "Pete Hunt",
+    salary: "67000.0",
+  },
+  3: {
+    name: "Pete sampras",
+    salary: "105540.0",
+  },
+  4: {
+    name: "Remy Sharp",
+    salary: "82220.0",
+  },
+  5: {
+    name: "David Beckham",
+    salary: "64430.0",
+    sleepHour: 4,
+  },
+  6: {
+    favLibrary: "React",
+    name: "A Random Guy",
+    pizzaSlice: 5,
+    salary: "36000.0",
+  },
+  7: {
+    name: "JS Dude",
+    salary: "44432",
+  },
+  8: {
+    name: "Jhankar Mahbub",
+    organization: "Nielsen",
+    salary: "53454",
+  },
+  9: {
+    name: "Jhankar Mahbub",
+    salary: "43454",
+  },
+  10: {
+    name: "Jhankar Mahbub",
+    salary: "33454",
+  },
+  11: {
+    name: "Jhankar Mahbub",
+    salary: "23444",
+  },
+  12: {
+    name: "Jhankar Mahbub",
+    salary: "63444",
+  },
+  13: {
+    name: "Jhankar Mahbub",
+    salary: "53655",
+  },
+};
